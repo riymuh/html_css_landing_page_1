36d45db25aa5be605afcdcfd073da44b9556b6b7
riyadh <mriyadh103@gmail.com>
2021-08-26 10:30:56 +0700

    first

---
+++ main.js
@@ -0,0 +1,9 @@
+function closeNewsletter() {
+  var x = document.getElementById("newsletter");
+  x.classList.toggle("newsletter-hidden");
+}
+
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
